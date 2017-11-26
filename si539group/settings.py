bb10eb46d0c8b006322986d2b362c61b05522e46
rivernews <classicalsupport_mrrivernews@hotmail.com>
2017-11-26 15:19:27 -0500

    fixing static file problems

--- si539group/settings.py
+++ si539group/settings.py
@@ -106,9 +106,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
 STATIC_URL = '/static/'
-
 
 ########################################################
 ## Settings for Local and Release                     ##
@@ -127,9 +125,13 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 try:
     from .local_settings import *
