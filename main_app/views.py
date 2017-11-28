b0141bfb894a67dd78f71b182c59241f22f65196
rivernews <classicalsupport_mrrivernews@hotmail.com>
2017-11-28 18:02:25 -0500

    fix

--- main_app/views.py
+++ main_app/views.py
@@ -5,12 +5,42 @@ from django.http import JsonResponse
 import os
 import csv
 
-def index(request):
+def getCSVdata():
+    # prepare python dict
     PROJECT_PATH = os.path.abspath(os.path.dirname(__name__))
     csv_filepath = os.path.join(PROJECT_PATH, 'fire-events_sortedbytime.csv')
+    row_data_list = []
+    nol_to_read = 10
+    i = 0
+    print('we got POST request in django')
+    with open(csv_filepath, newline='') as csvfile:
+        csv_dictreader = csv.DictReader(csvfile)
+        for row in csv_dictreader:
+            if (i<nol_to_read):
+                row_data_list.append({
+                    'time': row['yearmonth'],
+                    'lat': row['lat'],
+                    'long': row['long'],
+                    'area': row['acres_burned'],
+                    'name': ' or '.join( [ row['na_l3name'], row['na_l2name'], row['na_l1name'] ] ),
+                    'max_temp': row['max_air_temp'],
+                    'wind_speed': row['mean_wind_speed'],
+                    'mean_potential_et': row['mean_potential_et'],
+                    'total_precip' : row['total_precip'],
+                })
+            else:
+                break
+            i += 1
+    # convert to json
+    # ready to send json
+    return row_data_list
 
+def index(request):
     if request.method == 'GET':
-        vars = {'title': 'Home Page la'}
+        vars = {
+            'title': 'Home Page la',
+            'data': getCSVdata(),
+        }
         return render(request,'base.html',vars)
     elif request.method == 'POST':
         '''# prepare python dict
