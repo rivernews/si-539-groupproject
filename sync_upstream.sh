3f87b9b56babc80298e525033d9226fa4f975e9f
rivernews <classicalsupport_mrrivernews@hotmail.com>
2017-12-06 11:21:55 -0500

    Sync With Upstream by script

--- sync_upstream.sh
+++ sync_upstream.sh
@@ -2,5 +2,5 @@ git fetch upstream
 git merge upstream/master
 git checkout team-edit
 git add .
-git commit -m "SyncWithUpstream"
+git commit -m "Sync With Upstream by script"
 git push origin team-edit
